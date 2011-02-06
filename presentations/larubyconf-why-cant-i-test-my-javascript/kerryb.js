d2c97010780b5060f07f802ffede20a14878ae04
Gregory Moeck <gmoeck@gmail.com>
2011-02-05 16:09:05 -0800

    Add LARubyConf ligtning talk

---
+++ presentations/larubyconf-why-cant-i-test-my-javascript/kerryb.js
@@ -0,0 +1,8 @@
+/*jslint white: true, evil: true,browser: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
+/*global window: false, jQuery: false */
+"use strict";
+
+$(document).ready(function(){
+  SyntaxHighlighter.defaults["gutter"] = false;
+  SyntaxHighlighter.defaults["toolbar"] = false;
+});
